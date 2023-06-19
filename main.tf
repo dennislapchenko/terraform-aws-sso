9443fe4304361b959d61fbed7b52c8e40e23b822
Dennis Lapchenko <dennis.lapchenko@gmail.com>
2023-06-19 13:17:39 +0300

    Fix aws_identitystore_group and aws_identitystore_user to work with provider.aws v5+

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_ssoadmin_instances" "this" {}
 data "aws_identitystore_group" "this" {
   for_each          = toset(local.groups)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  filter {
+  attribute_path {
     attribute_path  = "DisplayName"
     attribute_value = each.value
   }
@@ -37,7 +37,7 @@ data "aws_identitystore_group" "this" {
 data "aws_identitystore_user" "this" {
   for_each          = toset(local.users)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  filter {
+  attribute_path {
     attribute_path  = "UserName"
     attribute_value = each.value
   }
